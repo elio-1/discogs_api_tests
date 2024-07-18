434115ad8d88fa0d0994c4c4529e01efa86300d8
elio-1 <69924027+elio-1@users.noreply.github.com>
2024-07-18 12:16:08 +0200

    search level 1

--- discogs_api_search.py
+++ discogs_api_search.py
@@ -1,3 +1,16 @@
-## curl "https://api.discogs.com/database/search?q=earth+wind+fire+september" -H "Authorization: Discogs token=TOKEN"
-## /database/search?q={query}&{?type,title,release_title,credit,artist,anv,label,genre,style,country,year,format,catno,barcode,track,submitter,contributor}
-## curl "https://api.discogs.com/database/search?q=earth+wind+fire+september&type=release" -H "Authorization: Discogs token=TOKEN"
+import discogs_client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv("DISCOGS_SECRET_KEY")
+d = discogs_client.Client('my_user_agent/1.0', user_token=token)
+search_input = input("Search: ")
+results = d.search(search_input, type="release")
+artists = results[0].credits
+print(artists)
+results2 = d.search(artists[-1].name, type="release")
+print(results2[0].title)
+print(results2[1].title)
+print(results2[2].title)
