2364679085f1f619aca7b4ea4306994e16b7de30
elio-1 <69924027+elio-1@users.noreply.github.com>
2024-07-19 13:25:59 +0200

    edit

--- suggestions.py
+++ suggestions.py
@@ -7,21 +7,19 @@ def load_api_key():
     load_dotenv()
     return os.getenv("DISCOGS_SECRET_KEY")
 
-
 def get_suggestions(search_input, how_many=3):
     search_input = search_input.lower()
     d = discogs_client.Client('my_user_agent/1.0', user_token=load_api_key())
     results = d.search(search_input, type="release")
     artists = results[0].credits
     if artists:
-        print(artists)
         i = 1
         while True:
             tmp_artist = artists[-i].name.lower().split()
-            pattern = fr"\b(?:{tmp_artist[0]})|(?:{tmp_artist[1]})\b"
-            print(tmp_artist)
-            print(pattern)
-            print(search_input)
+            if len(tmp_artist) > 1:
+                pattern = fr"\b(?:{tmp_artist[0]})|(?:{tmp_artist[1]})\b"
+            else:
+                pattern = fr"\b(?:{tmp_artist[0]})\b"
             if not re.search(pattern, search_input):
                 break
             if i > len(artists):
@@ -31,7 +29,7 @@ def get_suggestions(search_input, how_many=3):
         print("searching for: " + artists[-i].name)
         results2 = d.search(artists[-i].name, type="release")
         suggestions = [results2[i].title for i in list(range(how_many))]
-        print([suggestion for suggestion in suggestions])
+        print(suggestions)
         return suggestions
     else:
         raise Exception('no credit found, try to be more specific with the search input.')
