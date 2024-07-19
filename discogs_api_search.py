ede712a2b570a580d4ba2e39451af10e01b03b05
elio-1 <69924027+elio-1@users.noreply.github.com>
2024-07-19 11:15:01 +0200

    search for different artist

--- discogs_api_search.py
+++ discogs_api_search.py
@@ -1,6 +1,6 @@
-import discogs_client
 import os
 from dotenv import load_dotenv
+import discogs_client
 
 load_dotenv()
 
@@ -9,8 +9,17 @@ d = discogs_client.Client('my_user_agent/1.0', user_token=token)
 search_input = input("Search: ")
 results = d.search(search_input, type="release")
 artists = results[0].credits
-print(artists)
-results2 = d.search(artists[-1].name, type="release")
-print(results2[0].title)
-print(results2[1].title)
-print(results2[2].title)
+if artists:
+    print(artists)
+    i = 1
+    while True:
+        if bool(word not in artists[-1].name for word in search_input.split()):
+            break
+        i = 2
+    print("searching for: " + artists[-i].name)
+    results2 = d.search(artists[-i].name, type="release")
+    print(results2[0].title)
+    print(results2[1].title)
+    print(results2[2].title)
+else:
+    print('no credit found')
